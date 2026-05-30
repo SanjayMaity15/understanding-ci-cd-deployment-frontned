ce42ed2e3691dd9877db148009535b8532a3b36e
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-30 10:43:21 +0530

    test add

---
+++ test/dummy.test.js
@@ -0,0 +1,10 @@
+const shouldPassTest = true
+
+if (shouldPassTest) {
+    console.log("Test Passed successfully")
+    process.exit(0)
+} else {
+    console.log("Test failed successfully")
+    process.exit(1)
+}
+
